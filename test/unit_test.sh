1c27dc83d39f0a7e8e623fab449688e82213561d
David Walker <david.walker@quantifex.com>
2021-07-13 01:14:42 -0400

    GitHub action based unit testing

--- test/unit_test.sh
+++ test/unit_test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-pwd
-ls -lart
-
 RUSTFLAGS="-Zinstrument-coverage"
 cargo +nightly test
 grcov . --binary-path target/debug -s . -t html --branch --ignore-not-existing --excl-start "// GCOVR_EXCL_START" --excl-stop "// GCOVR_EXCL_STOP" -o ./coverage/
