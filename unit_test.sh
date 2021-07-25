19cc71637a84d1661d384d60a68138d6202726dd
David Walker <david.walker@quantifex.com>
2021-07-25 14:33:26 -0400

    Textual colorization

--- unit_test.sh
+++ unit_test.sh
@@ -7,10 +7,11 @@ export RUSTFLAGS="-Zinstrument-coverage"
 export CARGO_INCREMENTAL=0
 cargo +nightly test
 
-# Generate Code Coverage (in lcov for lcov and codecov usage)
+# Generate Code Coverage (in lcov for genhtml and codecov usage)
 mkdir -p ./coverage
 grcov . --binary-path target/debug -s . --ignore="/*" -t lcov --branch --ignore-not-existing -o ./coverage/lcov.info
 
+# Only run genhtml if it's installed (it's not included in the raw rust_devtest container)
 if [ $(command -v genhtml) ]; then
     genhtml ./coverage/lcov.info --output-directory coverage
 fi
